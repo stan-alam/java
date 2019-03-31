12596f2e4605313869b1a87fb84e8673c5927c18
Stan Alam <stan.alam@gmail.com>
2019-03-31 11:25:38 -0400

    fixed another grammar mistake in notes-code, and updated code to compile

--- OCA/OCPse7/exam2/01/src/GradeResultsAvg.java
+++ OCA/OCPse7/exam2/01/src/GradeResultsAvg.java
@@ -2,7 +2,7 @@ class GradeResultsAvg {
   public double calcAvg(int grade1, int grade2) {  //access is public
     return (grade1 + grade2) / 2;
   }
-  protected double calcAvg(int grade1, int grade2) { // acces is protected
-    return (grade1, grade2) / 2;
+  protected double calcAvg(int grade1, int grade2) { // access is protected
+    return (grade1 + grade2) / 2;
   }
 }
