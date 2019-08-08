49380fb051662aced631bafa9fcd5c5435e32431
stan.alam <stan.alam@gmail.com>
2019-08-08 18:28:41 +0000

    added OCA java programming

---
+++ OCA/java_programming/comp11/images/11/src/256/MultiPass.java
@@ -0,0 +1,8 @@
+//create more than 2 threads
+class NewThread implements Runnable {
+  String name;
+  Thread t;
+NewThread (String threadName) {
+} //end method threadName
+
+} //end class NewThread
