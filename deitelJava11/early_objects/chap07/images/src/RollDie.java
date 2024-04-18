1d3de9a2c269c856291e8aad07f9fa8298c68890
stanislaw alam <stan.alam@gmail.com>
2024-04-17 23:13:35 -0400

    added to java 11 - early objects, using elements of an array as counters

--- deitelJava11/early_objects/chap07/images/src/RollDie.java
+++ deitelJava11/early_objects/chap07/images/src/RollDie.java
@@ -1,7 +1,7 @@
 import java.security.SecureRandom;
 // 319..A program that simulates the rolling of a die 70,000,000 times and displays the frequency of each face.
 public class RollDie {
-    pubic static void main(String[] args)
+    public static void main(String[] args)
     {
         SecureRandom randomNumbs = new SecureRandom();
         int[] frequency = new int[7]; // array of freq counters
@@ -13,12 +13,12 @@ public class RollDie {
         }
     
 
-        System.out.println("%s%10s\n", "Face", "Frequency");
+        System.out.printf("%s%10s\n", "Face", "Frequency");
 
         //output each array element's value
         for (int face = 1; face < frequency.length; face++)
         {
-            System.out.println("%4d%10d\n", face, frequency[face]);
+            System.out.printf("%4d%10d\n", face, frequency[face]);
         }
 
     }
