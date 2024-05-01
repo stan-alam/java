1fea1a53cadf9eb5a1ceb422b24e89d9304f62e8
stanislaw alam <stan.alam@gmail.com>
2024-05-01 18:30:37 -0400

    added to CommissionedEmp.java

--- deitelJava11/early_objects/images/10/src/CommissionEmp.java
+++ deitelJava11/early_objects/images/10/src/CommissionEmp.java
@@ -20,4 +20,23 @@ public class CommissionEmp extends Emp {
         this.grossSales = grossSales;
         this.commissionRate = commissionRate;
     }
+    
+    //set the gross sales amount
+    public void setGrossSales(double grossSales){
+        if (grossSales < 0.0) { 
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        this.grossSales = grossSales;
+    }
+    //return gross sales amount
+    public double getGrossSales() {
+        return grossSales;
+    }
+//set the commission rate
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+    }
+
 }
