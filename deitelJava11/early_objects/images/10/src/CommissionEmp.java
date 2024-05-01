b1815a92588a969daeeb8e9d032fe1fb6c7d91e2
stanislaw alam <stan.alam@gmail.com>
2024-05-01 18:35:16 -0400

    added to CommissionedEmp.java

--- deitelJava11/early_objects/images/10/src/CommissionEmp.java
+++ deitelJava11/early_objects/images/10/src/CommissionEmp.java
@@ -37,6 +37,22 @@ public class CommissionEmp extends Emp {
         if (commissionRate <= 0.0 || commissionRate >= 1.0) {
             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
         }
+        this.commissionRate = commissionRate;
+    }
+    //return the commission rate
+    public double getCommissionRate() {
+        return commissionRate;
     }
 
+    //calculate earnings; meaning -> override abstract method earnings in Emp
+    @Override //indicates that this method overrides a superclass method
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    //return the String representation of CommissionEmp obj
+    @Override //indicates that this method overrides a superclass method
+    public String toString() {
+        return String.format("%s: %s%n%s: $%,.2f; %s: %.2f", "commission employee", super.toString(), "gross sales", getGrossSales(), "commission rate", getCommissionRate());
+    }
 }
