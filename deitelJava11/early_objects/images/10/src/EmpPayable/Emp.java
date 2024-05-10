c83b5ba92aa727d542796ee8de06f8eee72cc062
stanislaw alam <stan.alam@gmail.com>
2024-05-10 15:26:14 -0400

    added to abstract class Emp that implements Payable interface.'

--- deitelJava11/early_objects/images/10/src/EmpPayable/Emp.java
+++ deitelJava11/early_objects/images/10/src/EmpPayable/Emp.java
@@ -4,6 +4,22 @@ public abstract class Emp implements Payable {
     private final String SSN;
 
     public Emp(String fName, String lName, String SSN) {
-        
+        this.fName = fName;
+        this.lName = lName;
+        this.SSN = SSN;    
     }
+
+    // return first name
+    public String getFname() {
+        return fName;
+    }
+    //return last name
+    public String getLname() {
+        return lName;
+    }
+    //return SSN
+    public String getSSN() {
+        return SSN;
+    }
+
 }
