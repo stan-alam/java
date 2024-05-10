fe601ecc48eefba42d00f1531ecc0ba7ccda3227
stanislaw alam <stan.alam@gmail.com>
2024-05-10 15:18:08 -0400

    added to Invoice, implements payable interface, need to change params, has 'Code Smell.'

--- deitelJava11/early_objects/images/10/src/EmpPayable/Invoice.java
+++ deitelJava11/early_objects/images/10/src/EmpPayable/Invoice.java
@@ -20,4 +20,22 @@ public class Invoice implements Payable {
         this.partQnty = partQnty;
         this.pricePerItem = pricePerItem;
     }
+    //lets get the partNumber
+    public String getPartNum() {return partNum;}
+    //lets get the part description
+    public String getPartDeString() {return partDesc;}
+    //lets get the part quantity
+    public int getPartQnty() {return partQnty;}
+    //lets get the price per item
+    public double getPricePerItem() {return pricePerItem;}
+
+    @Override
+    public String toString() {
+        return String.format("%s: \n%s: %s (%s) \n%s: %d \n%s: $%,.2f", // CODE SMELL: Long Parameter List
+        "invoice", "part number", getPartNum(), getPartDeString(), "quantity", getPartQnty(), "price per item", getPricePerItem());
+    }   
+    @Override
+    public double getPaymentAmount() {
+        return getPartQnty() * getPricePerItem();
+    }
 }
