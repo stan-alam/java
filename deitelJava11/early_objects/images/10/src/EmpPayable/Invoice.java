93156167089fff3659571f9af2405df4e2cad531
stanislaw alam <stan.alam@gmail.com>
2024-05-10 15:02:39 -0400

    added to Invoice.Java implements Payable Interface

--- deitelJava11/early_objects/images/10/src/EmpPayable/Invoice.java
+++ deitelJava11/early_objects/images/10/src/EmpPayable/Invoice.java
@@ -1,6 +1,17 @@
 public class Invoice implements Payable {
     private final String partNum;
     private final String partDesc;
-    private final String partQuantity;
+    private final String partQnty;
     private final double pricePerItem;
+
+    // constructor
+    public Invoice(String partNum, String partDesc, int partQnty, double pricePerItem) {
+        //validate the partQnty
+        if (partQnty < 0) {
+            throw new IllegalArgumentException("Quantity must be >= 0");
+        }
+    }
+    if (pricePerItem < 0.0) {
+        throw new IllegalArgumentException("Price per item must be >= 0");
+    }
 }
