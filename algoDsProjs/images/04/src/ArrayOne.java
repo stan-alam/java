7990a5559e0a85eba1f771dfed6f91cee917e18c
stan alam <stan.alam@gmail.com>
2025-07-20 12:55:59 -0400

    added src to Java algo projects

---
+++ algoDsProjs/images/04/src/ArrayOne.java
@@ -0,0 +1,11 @@
+public class ArrayOne {
+public static boolean isSorted(int[] a){
+    for (int i = 0; i < a.length - 1; i++){
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
