a1540f90f545dcb92c5409d81a0e394d05607c0a
stan alam <stan.alam@gmail.com>
2025-12-24 18:57:27 -0500

    added to caesar cipher

---
+++ algoDsProjs/images/01/src/Cipher.java
@@ -0,0 +1,17 @@
+public class Cipher {
+
+    static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**
+     * we're gonna perform a Casear-style cipher rotation
+     * 
+     * @param ascii the input String of uppercase letters
+     * @param shift number of positions to rotate
+     * @return the encrypted String cipherascii
+     * 
+     */
+public static String encrypt(String ascii, int shift) {
+    
+}
+
+}
